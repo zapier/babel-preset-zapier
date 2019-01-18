20c423e49f3be50de05bf627538d50b0d409b5b5
ngryman <ngryman@gmail.com>
2019-01-18 12:13:50 +0100

    refact: extract the main config blocks into functions
    This just to reason a little better about the different chunks of the configuration.

--- index.js
+++ index.js
@@ -17,6 +17,61 @@ const normalizeOptions = (options = {}) => ({
   target: validateTargetOption(options.target),
 });
 
+const configurePresets = (env, target) =>
+  compact([
+    [
+      '@babel/preset-env',
+      {
+        modules: env === 'test' || target === 'node' ? 'commonjs' : false,
+        targets:
+          target === 'browser'
+            ? {
+                browsers: buildTargets,
+              }
+            : {
+                node: 'current',
+              },
+      },
+    ],
+    target === 'browser' && '@babel/preset-react',
+    '@babel/preset-flow',
+  ]);
+
+const configurePlugins = (env, target) =>
+  compact([
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-class-properties',
+    // Need to force-transform classes due to an Edge bug. See: https://github.com/zapier/zapier/pull/21319#issuecomment-437871079
+    // Once the fix for this lands on Edge, we can get rid of this (but make sure to test in older versions of Edge first!).
+    target === 'browser' && '@babel/plugin-transform-classes',
+    env === 'test' && 'babel-plugin-require-context-hook',
+  ]);
+
+const configureEnv = (env, target) => ({
+  development: {
+    plugins: compact([
+      target === 'browser' && [
+        'emotion',
+        {
+          autoLabel: true,
+          labelFormat: '[filename]--[local]',
+          sourceMap: true,
+        },
+      ],
+    ]),
+  },
+  test: {
+    plugins: compact([target === 'browser' && 'dynamic-import-node']),
+  },
+  production: {
+    plugins: compact([
+      target === 'browser' && 'transform-react-remove-prop-types',
+      'graphql-tag',
+    ]),
+  },
+});
+
 module.exports = declare((api, options) => {
   const env = process.env.BABEL_ENV || process.env.NODE_ENV;
   const { target } = normalizeOptions(options);
@@ -27,55 +82,8 @@ module.exports = declare((api, options) => {
   api.cache.using(() => env);
 
   return {
-    presets: compact([
-      [
-        '@babel/preset-env',
-        {
-          modules: env === 'test' || target === 'node' ? 'commonjs' : false,
-          targets:
-            target === 'browser'
-              ? {
-                  browsers: buildTargets,
-                }
-              : {
-                  node: 'current',
-                },
-        },
-      ],
-      target === 'browser' && '@babel/preset-react',
-      '@babel/preset-flow',
-    ]),
-    plugins: compact([
-      '@babel/plugin-syntax-dynamic-import',
-      '@babel/plugin-proposal-object-rest-spread',
-      '@babel/plugin-proposal-class-properties',
-      // Need to force-transform classes due to an Edge bug. See: https://github.com/zapier/zapier/pull/21319#issuecomment-437871079
-      // Once the fix for this lands on Edge, we can get rid of this (but make sure to test in older versions of Edge first!).
-      target === 'browser' && '@babel/plugin-transform-classes',
-      env === 'test' && 'babel-plugin-require-context-hook',
-    ].filter(Boolean),
-    env: {
-      development: {
-        plugins: compact([
-          target === 'browser' && [
-            'emotion',
-            {
-              autoLabel: true,
-              labelFormat: '[filename]--[local]',
-              sourceMap: true,
-            },
-          ],
-        ]),
-      },
-      test: {
-        plugins: compact([target === 'browser' && 'dynamic-import-node']),
-      },
-      production: {
-        plugins: compact([
-          target === 'browser' && 'transform-react-remove-prop-types',
-          'graphql-tag',
-        ]),
-      },
-    },
+    presets: configurePresets(env, target),
+    plugins: configurePlugins(env, target),
+    env: configureEnv(env, target),
   };
 });
